7609fd546c24c898e90fcf42c764b101557d5c86
Leutrim <leutrim@example.com>
2026-03-06 04:18:03 +0100

    Add Swagger Newtonsoft support

--- FlightReservation.API/Program.cs
+++ FlightReservation.API/Program.cs
@@ -6,26 +6,13 @@ using FlightReservation.Infrastructure.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers()
-    .AddNewtonsoftJson();
+    .AddNewtonsoftJson(options =>
+    {
+        options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+    });
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
-    {
-        Title = "Flight Reservation API",
-        Version = "v1",
-        Description = "Onion Architecture - Flight Reservation System"
-    });
-});
-
-builder.Services.AddScoped<IFlightRepository, FlightRepository>();
-builder.Services.AddScoped<IReservationRepository, ReservationRepository>();
-builder.Services.AddScoped<IEmailService, EmailService>();
-builder.Services.AddScoped<IPaymentService, PaymentService>();
-
-builder.Services.AddScoped<SearchFlightsUseCase>();
-builder.Services.AddScoped<CreateReservationUseCase>();
+builder.Services.AddSwaggerGenNewtonsoftSupport();
 
 var app = builder.Build();
 
