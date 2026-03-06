f1947ae4ba8f4c8bb41ea86b73de0c0e67716d51
Leutrim <leutrim@example.com>
2026-03-06 04:10:54 +0100

    Fix JSON serialization with NewtonsoftJson

--- FlightReservation.API/Program.cs
+++ FlightReservation.API/Program.cs
@@ -5,7 +5,9 @@ using FlightReservation.Infrastructure.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddNewtonsoftJson();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
